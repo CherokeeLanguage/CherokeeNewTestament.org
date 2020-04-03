45d4efd258e107d31736af7df486ed3c3bd28016
Michael Conrad <m.conrad.202@gmail.com>
2020-04-03 13:28:43 -0400

    Prep for initial deployment.

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+trap "echo ERROR; read a" ERR
+
+if [ "$(hostname)" != "Precision-Workstation" ]; then echo "WRONG MACHINE"; exit -1; fi
+
+cd "$(dirname "$0")"
+
+rm -rf public
+zola build
+
+FROM="./public/"
+
+cp content/.htaccess "$FROM/"
+
+DEST="clcom@cherokeelessons.com:./cnt/public_html/"
+rsync --progress -a --delete-after -z --verbose --human-readable "$FROM" "$DEST"
